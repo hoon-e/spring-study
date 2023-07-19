caf385600eff592ff453ef437cadcef644fc6ade
jaehoon <jaehoon@jaehoons-MacBook-Pro.local>
2023-07-19 23:39:46 +0900

    2023-07-18 : 학습 내용 추가

--- learn-spring-framework/src/main/java/me/jaehoon/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/me/jaehoon/learnspringframework/HelloWorldConfiguration.java
@@ -3,8 +3,8 @@ package me.jaehoon.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person (String name, int age) {}
 record Address (String firstLine, String city) {}
+record Person (String name, int age, Address address) {}
 
 @Configuration
 public class HelloWorldConfiguration {
@@ -20,11 +20,31 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        return new Person("jaehoon", 28);
+        return new Person("jaehoon", 28, new Address("cheongju", "city"));
     }
 
-    @Bean
+    @Bean(name="address2")
     public Address address() {
         return new Address("seoul", "gwanak");
+    }
+
+    // 이 부분에서 에러가 발생한다.
+    @Bean(name="address3")
+    public Address address3() {
+        return new Address("baker street", "london");
+    }
+
+    // name과 age bean을 사용하게 하려면 어떻게 하면될까?
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());
+    }
+
+    // 매개변수로 역시 적절한 bean을 재활용할 수 있다.
+    // 매개변수의 이름은 메서드 이름과 동일해야 한다.
+    // 이때는 메서드 호출이 필요없고, 매개변수로 넘어온 값을 사용해주면 된다.
+    @Bean
+    public Person person3MethodCall(String name, int age, Address address2) {
+        return new Person(name, age, address2);
     }
 }
--- learn-spring-framework/src/main/java/me/jaehoon/learnspringframework/Main02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/me/jaehoon/learnspringframework/Main02HelloWorldSpring.java
@@ -1,8 +1,15 @@
 package me.jaehoon.learnspringframework;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class Main02HelloWorldSpring {
+    private static final LoggerContext ctx = (LoggerContext) LoggerFactory.getILoggerFactory();
+    private static final Logger logger = ctx.getLogger(Main02HelloWorldSpring.class);
+
     public static void main(String[] args) {
         // 1. Spring Context를 실행
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
@@ -12,12 +19,19 @@ public class Main02HelloWorldSpring {
         // name - @Bean
 
         // 3. Spring에서 관리되는 Bean을 반환한다.
-        System.out.println(context.getBean("name"));
+        logger.info(context.getBean("name").toString());
 
-        System.out.println(context.getBean("age"));
+        logger.info(context.getBean("age").toString());
 
-        System.out.println(context.getBean("person"));
+        logger.info(context.getBean("person").toString());
 
-        System.out.println(context.getBean("address"));
+        logger.info(context.getBean("address2").toString());
+
+        // context의 type도 사용할 수 있다.
+        logger.info(context.getBean(Address.class).toString());
+
+        logger.info(context.getBean("person2MethodCall").toString());
+
+        logger.info(context.getBean("person3MethodCall").toString());
     }
 }
